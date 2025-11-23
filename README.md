e73273e61c547de3d95921e49788a6b187274698
Meet Patel <meetp4499@gmail.com>
2025-11-23 20:34:16 +0530

    Final Commit

--- README.md
+++ README.md
@@ -65,20 +65,20 @@ open up you local host and port
 ### dagshub
 [dagshub](https://dagshub.com/)
 
-MLFLOW_TRACKING_URI=https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow \
-MLFLOW_TRACKING_USERNAME=entbappy \
-MLFLOW_TRACKING_PASSWORD=6824692c47a369aa6f9eac5b10041d5c8edbcef0 \
+MLFLOW_TRACKING_URI=https://dagshub.com/meet1214/kidney-disease-classification.mlflow \
+MLFLOW_TRACKING_USERNAME=meet1214 \
+MLFLOW_TRACKING_PASSWORD=6af09b035f6ae71b885643798dcc7690af76f5a5 \
 python script.py
 
 Run this to export as env variables:
 
 ```bash
 
-export MLFLOW_TRACKING_URI=https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow
+export MLFLOW_TRACKING_URI=https://dagshub.com/meet1214/kidney-disease-classification.mlflow
 
-export MLFLOW_TRACKING_USERNAME=entbappy 
+export MLFLOW_TRACKING_USERNAME=meet1214 
 
-export MLFLOW_TRACKING_PASSWORD=6824692c47a369aa6f9eac5b10041d5c8edbcef0
+export MLFLOW_TRACKING_PASSWORD=6af09b035f6ae71b885643798dcc7690af76f5a5
 
 ```
 
--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -11,24 +11,19 @@ class Evaluation:
     def __init__(self, config: EvaluationConfig):
         self.config = config
 
-    
     def _valid_generator(self):
-
         datagenerator_kwargs = dict(
             rescale = 1./255,
             validation_split=0.30
         )
-
         dataflow_kwargs = dict(
             target_size=self.config.params_image_size[:-1],
             batch_size=self.config.params_batch_size,
             interpolation="bilinear"
         )
-
         valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
             **datagenerator_kwargs
         )
-
         self.valid_generator = valid_datagenerator.flow_from_directory(
             directory=self.config.training_data,
             subset="validation",
@@ -36,14 +31,23 @@ class Evaluation:
             **dataflow_kwargs
         )
 
-
     @staticmethod
     def load_model(path: Path) -> tf.keras.Model:
         return tf.keras.models.load_model(path)
-    
 
     def evaluation(self):
         self.model = self.load_model(self.config.path_of_model)
+        try:
+            is_compiled = getattr(self.model, "_is_compiled", False)
+        except Exception:
+            is_compiled = False
+        if not is_compiled:
+            lr = float(self.config.all_params.LEARNING_RATE)
+            self.model.compile(
+                optimizer=tf.keras.optimizers.SGD(learning_rate=lr),
+                loss=tf.keras.losses.CategoricalCrossentropy(),
+                metrics=["accuracy"]
+            )
         self._valid_generator()
         self.score = self.model.evaluate(self.valid_generator)
         self.save_score()
@@ -52,23 +56,25 @@ class Evaluation:
         scores = {"loss": self.score[0], "accuracy": self.score[1]}
         save_json(path=Path("scores.json"), data=scores)
 
-    
     def log_into_mlflow(self):
         mlflow.set_registry_uri(self.config.mlflow_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        
+        import tensorflow as tf
+        if not hasattr(tf.keras, "__version__"):
+            import tensorflow as tf_main
+            tf.keras.__version__ = tf_main.__version__
         with mlflow.start_run():
             mlflow.log_params(self.config.all_params)
-            mlflow.log_metrics(
-                {"loss": self.score[0], "accuracy": self.score[1]}
-            )
-            # Model registry does not work with file store
+            mlflow.log_metrics({"loss": self.score[0], "accuracy": self.score[1]})
             if tracking_url_type_store != "file":
-
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
+                try:
+                    mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
+                except Exception as e:
+                    from mlflow.exceptions import MlflowException
+                    if isinstance(e, MlflowException) and "403" in str(e):
+                        print("Registry not permitted, logging model without registration.")
+                        mlflow.keras.log_model(self.model, "model")
+                    else:
+                        raise e
             else:
                 mlflow.keras.log_model(self.model, "model")
